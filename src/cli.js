600f24a62a7bbf2263b8976ca199a7718b034c01
Masha <nasonkina@yandex.ru>
2022-01-16 01:54:46 +0300

    fix src/cli.js, format option & arguments

--- src/cli.js
+++ src/cli.js
@@ -4,7 +4,10 @@ const cli = () => {
   const program = new Command();
   program
     .description('Compares two configuration files and shows a difference.')
-    .version('1.0.0', '-V, --version', 'output the version number');
+    .argument('<filepath1>')
+    .argument('<filepath2>')
+    .version('1.0.0', '-V, --version', 'output the version number')
+    .option('-f, --format [type]', 'output format');
   program.parse();
 };
 
