aaeb78b9ae59b97b0762204cf46b8ef13a12acbe
Masha <nasonkina@yandex.ru>
2022-01-17 16:56:50 +0300

    fix compareObjects genDiff, returns string output

---
+++ genDiff.js
@@ -0,0 +1,20 @@
+import getJSON from './src/getJSON.js';
+import compareObjects from './src/compareObjects.js';
+
+/**
+ * @param {string} filepath1
+ * @param {string} filepath2
+ * @returns {string | Error}
+ */
+const genDiff = (filepath1, filepath2) => {
+  try {
+    const file1 = getJSON(filepath1);
+    const file2 = getJSON(filepath2);
+    // console.log(compareObjects(JSON.parse(file1), JSON.parse(file2)));
+    return compareObjects(JSON.parse(file1), JSON.parse(file2));
+  } catch (err) {
+    return err.message;
+  }
+};
+
+export default genDiff;
--- src/compareObjects.js
+++ src/compareObjects.js
@@ -1,59 +1,48 @@
-import { readFileSync } from 'fs';
-// import path from 'path';
 import _ from 'lodash';
 
 /**
  *
  * @param {Object} obj1
  * @param {Object} obj2
- * @returns {Array}
+ * @returns {string}
  */
 const compareObjects = (obj1, obj2) => {
   const mergedData = { ...obj2, ...obj1 };
-  const diffArr = Object.entries(mergedData)
+  // console.log('merged', mergedData);
+  const sorted = _.sortBy(Object.entries(mergedData), ([key]) => key);
+  // console.log('sorted', sorted);
+  const diffArr = sorted
     .map((el) => {
-      const diffResult = {
-        data: el,
-        removed: false,
-        added: false,
-        changed: false,
-      };
+      // const diffResult = {
+      //   data: el,
+      //   removed: false,
+      //   added: false,
+      //   changed: false,
+      // };
 
       const [key, value] = el;
 
       if (!_.has(obj2, key)) {
-        diffResult.removed = true;
+        return `- ${key}: ${value}`;
+        // diffResult.removed = true;
       }
 
       if (!_.has(obj1, key)) {
-        diffResult.added = true;
+        // diffResult.added = true;
+        return `+ ${key}: ${value}`;
       }
 
       if (_.has(obj2, key) && obj2[key] !== value) {
-        diffResult.changed = true;
-        diffResult.newData = { key: obj2[key] };
+        // diffResult.changed = true;
+        // diffResult.newData = { key: obj2[key] };
+        return [`- ${key}: ${value}`, `+ ${key}: ${obj2[key]}`].join('\n');
       }
-
-      return diffResult;
+      return `  ${key}: ${value}`;
     });
   // console.log(obj1);
   // console.log(typeof obj2);
-  console.log(diffArr);
-  return diffArr;
+  const strResult = `{\n${diffArr.join('\n')}\n}`;
+  return strResult;
 };
 
-// compareFiles(readFileSync(new URL('./fixtures/file1.json', import.meta.url)),
-// readFileSync(new URL('./fixtures/file2.json', import.meta.url)));
-
-// const file1 = readFileSync('../fixtures/file2.json');
-// console.log(import.meta.url);
-// const filepath1 = `${process.cwd()}/fixtures/file1.json`;
-// const filepath2 = `${process.cwd()}/fixtures/file2.json`;
-const filepath1 = './src/fixtures/file1.json';
-const filepath2 = './src/fixtures/file2.json';
-// const filepath1 = path.join
-compareObjects(JSON.parse(readFileSync(filepath1, 'utf8')), JSON.parse(readFileSync(filepath2, 'utf8')));
-
-// console.log(path.resolve());
-// console.log(process.cwd());
-// export default compareObjects;
+export default compareObjects;
