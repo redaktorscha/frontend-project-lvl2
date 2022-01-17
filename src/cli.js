0f72a040030add35462ab266495a0345b902ef82
Masha <nasonkina@yandex.ru>
2022-01-17 16:58:15 +0300

    fix src/cli.js, parse arguments, action handler

--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,5 @@
 import { Command } from 'commander';
+import genDiff from '../genDiff.js';
 
 const cli = () => {
   const program = new Command();
@@ -7,8 +8,14 @@ const cli = () => {
     .argument('<filepath1>')
     .argument('<filepath2>')
     .version('1.0.0', '-V, --version', 'output the version number')
-    .option('-f, --format [type]', 'output format');
+    .option('-f, --format [type]', 'output format')
+    .action((path1, path2) => {
+      console.log(genDiff(path1, path2));
+    });
   program.parse();
+
+  const userArgs = process.argv.slice(2);
+  return genDiff(...userArgs);
 };
 
 export default cli;
