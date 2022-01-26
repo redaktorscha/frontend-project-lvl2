e22c6ba46cc49cb8e605c17b910816a52582f3ba
Masha <nasonkina@yandex.ru>
2022-01-26 08:16:25 +0300

    fix compareObjects, compare func

--- src/compareObjects.js
+++ src/compareObjects.js
@@ -2,32 +2,38 @@ import _ from 'lodash';
 
 /**
  *
+ * @param {Array} elem
+ * @param {Object} object1
+ * @param {Object} object2
+ * @returns {string}
+ */
+export const compare = (elem, object1, object2) => {
+  const [key, value] = elem;
+
+  if (!_.has(object2, key)) {
+    return `- ${key}: ${value}`;
+  }
+
+  if (!_.has(object1, key)) {
+    return `+ ${key}: ${value}`;
+  }
+
+  if (_.has(object2, key) && object2[key] !== value) {
+    return [`- ${key}: ${value}`, `+ ${key}: ${object2[key]}`].join('\n');
+  }
+  return `  ${key}: ${value}`;
+};
+
+/**
+ *
  * @param {Object} obj1
  * @param {Object} obj2
  * @returns {string}
  */
-const compareObjects = (obj1, obj2) => {
+export const compareObjects = (obj1, obj2) => {
   const mergedData = { ...obj2, ...obj1 };
   const sorted = _.sortBy(Object.entries(mergedData), ([key]) => key);
   const diffArr = sorted
-    .map((el) => {
-      const [key, value] = el;
-
-      if (!_.has(obj2, key)) {
-        return `- ${key}: ${value}`;
-      }
-
-      if (!_.has(obj1, key)) {
-        return `+ ${key}: ${value}`;
-      }
-
-      if (_.has(obj2, key) && obj2[key] !== value) {
-        return [`- ${key}: ${value}`, `+ ${key}: ${obj2[key]}`].join('\n');
-      }
-      return `  ${key}: ${value}`;
-    });
-  const strResult = `{\n${diffArr.join('\n')}\n}`;
-  return strResult;
+    .map((el) => compare(el, obj1, obj2));
+  return `{\n${diffArr.join('\n')}\n}`;
 };
-
-export default compareObjects;
