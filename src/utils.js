9b37521d94815fdd6602613e2a94a4d99af8de5c
Masha <nasonkina@yandex.ru>
2022-01-28 00:14:06 +0300

    add getAbsPath func

--- src/utils.js
+++ src/utils.js
@@ -1,19 +1,25 @@
-import fs from 'fs';
+import { readFileSync } from 'fs';
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+
 /**
- *
  * @param {string} filename
  * @returns {string}
  */
 export const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
 /**
- *
- * @param {string} filename
- * @returns {JSON|string}
+ * @param {string} filepath
+ * @returns {string}
  */
-export const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+export const getAbsFilePath = (filepath) => path.resolve(filepath);
+
+/**
+ * @param {string} file name or path
+ * @param {Function} fn
+ * @returns {string}
+ */
+export const readFile = (file, fn) => readFileSync(fn(file), 'utf-8');
