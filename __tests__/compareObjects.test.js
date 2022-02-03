899267d35e315fce0d42d6cd47db830d5e66850c
Masha <nasonkina@yandex.ru>
2022-02-04 00:36:11 +0300

    fix tests

--- __tests__/compareObjects.test.js
+++ __tests__/compareObjects.test.js
@@ -1,11 +1,11 @@
 import { it, expect } from '@jest/globals';
 import { getFixturePath, readFile } from '../src/utils.js';
 import compareObjects from '../src/compareObjects.js';
+import diffArr from '../__fixtures__/fixtureArr.js';
 
-it('should return diff string if 2 obj given', () => {
+it('should return diff array if 2 obj given', () => {
   const obj1 = JSON.parse(readFile('file1.json', getFixturePath));
   const obj2 = JSON.parse(readFile('file2.json', getFixturePath));
-  const fixtureStr = readFile('diff', getFixturePath);
   const result = compareObjects(obj1, obj2);
-  expect(result).toBe(fixtureStr);
+  expect(result).toEqual(diffArr);
 });
