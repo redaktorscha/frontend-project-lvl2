9544c8fb155f061cc50cfb8ab2cc61f17e394fa0
Masha <nasonkina@yandex.ru>
2022-02-17 01:35:46 +0300

    fix genDiff (error handling)

--- src/genDiff.js
+++ src/genDiff.js
@@ -8,16 +8,12 @@ import chooseFormatter from './formatters/index.js';
  */
 const genDiff = (filepath1, filepath2, formatterName = 'stylish') => {
   try {
-    if (!filepath2 || (!filepath1 && !filepath2)) {
-      throw new Error('Missing required argument(s) <filepath1> <filepath2>');
-    }
-
     const obj1 = getParsedObject(filepath1);
     const obj2 = getParsedObject(filepath2);
     const formatter = chooseFormatter(formatterName);
     return formatter(compareObjects(obj1, obj2));
   } catch (err) {
-    return err.message;
+    throw new Error(err.message);
   }
 };
 
