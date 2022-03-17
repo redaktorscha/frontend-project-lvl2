d7cbc325d7a116013ffeae4c778317d457ce25f8
Masha <nasonkina@yandex.ru>
2022-03-17 04:36:24 +0300

    fix json formatter

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,11 +1,10 @@
 import stylish from './stylish.js';
 import plain from './plain.js';
-import json from './json.js';
 
 const formatters = {
   stylish,
   plain,
-  json,
+  json: JSON.stringify,
 };
 
 /**
